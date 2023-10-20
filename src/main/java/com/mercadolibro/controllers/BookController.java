f9bfa26755345244b48f59a72ec586c7d3cc289e
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2023-10-19 23:02:09 -0300

    feat: implemented book delete

--- src/main/java/com/mercadolibro/controllers/BookController.java
+++ src/main/java/com/mercadolibro/controllers/BookController.java
@@ -1,9 +1,29 @@
 package com.mercadolibro.controllers;
 
+import com.mercadolibro.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
+        try{
+            bookService.delete(id);
+            return ResponseEntity.noContent().build();
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
+    }
 }
--- src/main/java/com/mercadolibro/service/impl/BookServiceImpl.java
+++ src/main/java/com/mercadolibro/service/impl/BookServiceImpl.java
@@ -1,7 +1,24 @@
 package com.mercadolibro.service.impl;
 
+import com.mercadolibro.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BookServiceImpl {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void delete(Long id) throws Exception {
+        if (!bookRepository.existsById(id)) {
+            throw new Exception("Book not found");
+        }
+
+        bookRepository.deleteById(id);
+    }
+
 }
