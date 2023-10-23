89826fef843d8f2aec77d3a8ba7a01fda1aa3e93
lihuelmujica <lihuelmujica@gmail.com>
2023-10-23 15:28:14 -0300

    feat: add User and Role repositories

---
+++ src/main/java/com/mercadolibro/repositories/AppUserRepository.java
@@ -0,0 +1,7 @@
+package com.mercadolibro.repositories;
+
+import com.mercadolibro.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+}
