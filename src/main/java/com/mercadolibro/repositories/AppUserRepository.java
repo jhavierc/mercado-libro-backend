daaaae68072a6b5a414261369f1e9bb601a68f0f
lihuelmujica <lihuelmujica@gmail.com>
2023-10-23 19:02:54 -0300

    feat: implement UserService

--- src/main/java/com/mercadolibro/repositories/AppUserRepository.java
+++ src/main/java/com/mercadolibro/repositories/AppUserRepository.java
@@ -4,4 +4,5 @@ import com.mercadolibro.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+    boolean existsByEmail(String email);
 }
