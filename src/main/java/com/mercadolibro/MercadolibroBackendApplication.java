05b67bcda080658ca0b85c4f3153e74f7e8a27aa
lihuelmujica <lihuelmujica@gmail.com>
2023-10-24 15:26:34 -0300

    fix: set locale to english

--- src/main/java/com/mercadolibro/MercadolibroBackendApplication.java
+++ src/main/java/com/mercadolibro/MercadolibroBackendApplication.java
@@ -3,10 +3,13 @@ package com.mercadolibro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class MercadolibroBackendApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.ENGLISH);
 		SpringApplication.run(MercadolibroBackendApplication.class, args);
 	}
 
