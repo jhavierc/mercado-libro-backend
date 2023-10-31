dc60359d75960cf99140c5a5887a807c0941d79f
lihuelmujica <lihuelmujica@gmail.com>
2023-10-31 15:36:29 -0300

    docs: add examples to AuthResp and AuthReq

--- src/main/java/com/mercadolibro/dto/AuthReq.java
+++ src/main/java/com/mercadolibro/dto/AuthReq.java
@@ -1,9 +1,12 @@
 package com.mercadolibro.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class AuthReq {
+    @ApiModelProperty(value = "Email of the user", required = true, example = "juan@example.com")
     private String email;
+    @ApiModelProperty(value = "Password of the user", required = true, example = "123456")
     private String password;
 }
--- src/main/java/com/mercadolibro/dto/AuthResp.java
+++ src/main/java/com/mercadolibro/dto/AuthResp.java
@@ -1,11 +1,14 @@
 package com.mercadolibro.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class AuthResp {
+    @ApiModelProperty(value = "Token of the user", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWFuQGV4YW1wbGUuY29tIiwicm9sZXMiOlsiUk9MRV9VU0")
     private String token;
+    @ApiModelProperty(value = "User")
     private UserDTO user;
 }
