f48316b7b50d68e1d952d9368177ca87e92263ca
lihuelmujica <lihuelmujica@gmail.com>
2023-10-30 22:42:45 -0300

    add: get profile endpoint for user using jwt token

--- src/main/java/com/mercadolibro/controller/UserController.java
+++ src/main/java/com/mercadolibro/controller/UserController.java
@@ -5,23 +5,30 @@ import com.mercadolibro.exception.ResourceNotFoundException;
 import com.mercadolibro.dto.UserDTO;
 import com.mercadolibro.dto.UserRegisterDTO;
 import com.mercadolibro.service.UserService;
+import com.mercadolibro.util.JwtUtil;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import io.swagger.v3.oas.annotations.headers.Header;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 public class UserController {
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping
@@ -37,5 +44,16 @@ public class UserController {
     )
     public ResponseEntity<UserDTO> createUser(@RequestBody UserRegisterDTO userRegisterDTO) throws ResourceAlreadyExistsException, ResourceNotFoundException {
         return new ResponseEntity<>(userService.create(userRegisterDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Get user by token", notes = "Returns the user", authorizations = {@Authorization(value = "JWT Token")})
+    public ResponseEntity<UserDTO> getUser(HttpServletRequest request) throws ResourceNotFoundException {
+        String token = request.getHeader("Authorization");
+        if (token == null ) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        String jwt = token.substring(7);
+        String email = jwtUtil.extractUsername(jwt);
+        UserDTO user = userService.findByEmail(email);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
