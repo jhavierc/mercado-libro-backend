4d0ab0c3a7e524fc90a1430cdabff0598a10d3fc
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2023-11-01 00:16:44 -0300

    refactor: added prefix api to category controller

--- src/main/java/com/mercadolibro/controller/CategoryController.java
+++ src/main/java/com/mercadolibro/controller/CategoryController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/category")
+@RequestMapping("/api/category")
 @CrossOrigin(origins = "*")
 public class CategoryController {
     private final CategoryService categoryService;
