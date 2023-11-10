f6e0b5a9b784ebdd64f5b2b8d4dd914d5e5c6e8d
Nicolas Gambino <100079145+NicoGamboa@users.noreply.github.com>
2023-11-10 15:27:53 -0300

    Update BookRepository.java

--- src/main/java/com/mercadolibro/repository/BookRepository.java
+++ src/main/java/com/mercadolibro/repository/BookRepository.java
@@ -7,6 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.domain.Page;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     @Query("SELECT b.imageLinks FROM Book b WHERE b.id = :id")
@@ -15,4 +21,7 @@ public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificat
     boolean existsByIsbn(String isbn);
 
     boolean existsByIsbnAndIdNot(String isbn, Long id);
+
+    @Query("SELECT b FROM Book b WHERE b.title LIKE %:keyword% OR b.description LIKE %:keyword%")
+    Page<Book> findByTitleOrDescriptionContaining(String keyword, Pageable pageable);
 }
