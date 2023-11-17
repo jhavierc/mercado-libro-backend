407b23560ce4afec95b9ec51ee096f4aceeec6aa
lihuelmujica <lihuelmujica@gmail.com>
2023-11-17 20:18:38 -0300

    feat: add methods to authenticate with external providers in AuthController

--- src/main/java/com/mercadolibro/controller/AuthController.java
+++ src/main/java/com/mercadolibro/controller/AuthController.java
@@ -3,10 +3,14 @@ package com.mercadolibro.controller;
 import com.mercadolibro.dto.AuthReq;
 import com.mercadolibro.dto.AuthResp;
 import com.mercadolibro.dto.UserDTO;
+import com.mercadolibro.exception.ResourceAlreadyExistsException;
 import com.mercadolibro.exception.ResourceNotFoundException;
+import com.mercadolibro.exception.UnauthorizedException;
+import com.mercadolibro.service.OauthService;
 import com.mercadolibro.service.UserService;
 import com.mercadolibro.util.JwtUtil;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,12 +29,14 @@ import javax.servlet.http.HttpServletRequest;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
+    private final OauthService oauthService;
     private final JwtUtil jwtUtil;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager, UserService userService, JwtUtil jwtUtil) {
+    public AuthController(AuthenticationManager authenticationManager, UserService userService, OauthService oauthService, JwtUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
         this.userService = userService;
+        this.oauthService = oauthService;
         this.jwtUtil = jwtUtil;
     }
 
@@ -58,4 +64,23 @@ public class AuthController {
         UserDTO user = userService.findByEmail(email);
         return new ResponseEntity<>(new AuthResp(jwt, user), HttpStatus.OK);
     }
+
+    @GetMapping("/oauth/{provider}")
+    @ApiOperation(
+            value = "Get the URL to redirect the user to in order to start the OAuth2 flow",
+            notes = "Returns the URL to redirect the user to"
+    )
+    public ResponseEntity<String> loginUrl(@ApiParam(name = "provider", value = "Supported google or facebook", required = true, defaultValue = "google") @PathVariable String provider) throws ResourceNotFoundException {
+        return ResponseEntity.ok(oauthService.loginUrl(provider));
+    }
+
+    @PostMapping("/oauth/{provider}")
+    @ApiOperation(
+            value = "Login with OAuth2",
+            notes = "Returns the user")
+
+    public ResponseEntity<AuthResp> google(@ApiParam(name = "code", value = "The code returned by the OAuth2 provider", required = true) @RequestParam String code, @ApiParam(name = "provider", value = "Supported google or facebook", required = true, example = "google", defaultValue = "google") @PathVariable String provider) throws ResourceNotFoundException, UnauthorizedException, ResourceAlreadyExistsException {
+        return new ResponseEntity<>(oauthService.loginWithAuthorizationCode(provider, code), HttpStatus.OK);
+    }
+
 }
