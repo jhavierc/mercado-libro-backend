2721f1493bba0baa7fb425e536d54f3b5ba8b347
LihuelMujica <lihuelmujica@gmail.com>
2023-11-28 16:28:55 -0300

    feat: add findInvoicesAddressByUserEmail to InvoiceRepository

--- src/main/java/com/mercadolibro/repository/InvoiceRepository.java
+++ src/main/java/com/mercadolibro/repository/InvoiceRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
