2530f463d6160bf9a770772d3983110b90621cf8
LihuelMujica <lihuelmujica@gmail.com>
2023-11-30 17:18:37 -0300

    test: add shouldUpdateAddressWhenUserExistsAndAddressExists

--- src/test/java/com/mercadolibro/service/impl/AddressServiceImplTest.java
+++ src/test/java/com/mercadolibro/service/impl/AddressServiceImplTest.java
@@ -72,5 +72,52 @@ class AddressServiceImplTest {
         assertEquals(user.getId(), addressCreated.getUserId());
     }
 
+    @Test
+    void shouldUpdateAddressWhenUserExistsAndAddressExists() throws ResourceNotFoundException, ResourceAlreadyExistsException {
+        // GIVEN
+        AddressCreateDTO addressCreateDTO = AddressCreateDTO.builder()
+                .street("street")
+                .city("city")
+                .number(1)
+                .zipCode("zipCode")
+                .state("state")
+                .build();
+        String userEmail = "userEmail";
+        UserDTO user = new UserDTO();
+        user.setId(1);
+        user.setEmail(userEmail);
+        Address oldAddress = Address.builder()
+                .id(1)
+                .street("oldStreet")
+                .city("oldCity")
+                .number(1)
+                .zipCode("oldZipCode")
+                .state("oldState")
+                .userId(user.getId())
+                .build();
+
+        // WHEN
+        when(userService.findByEmail(userEmail)).thenReturn(user);
+        when(addressRepository.findByUserId(user.getId())).thenReturn(Optional.of(oldAddress));
+        when(addressRepository.save(any(Address.class))).thenAnswer(invocation -> {
+            Address address = invocation.getArgument(0);
+            if (address.getId() == null) {
+                address.setId(3);
+            }
+            return address;
+        });
+
+        // THEN
+        Address addressCreated = addressService.create(addressCreateDTO, userEmail);
+        assertNotNull(addressCreated);
+        assertEquals(oldAddress.getId(), addressCreated.getId());
+        assertEquals(addressCreateDTO.getStreet(), addressCreated.getStreet());
+        assertEquals(addressCreateDTO.getCity(), addressCreated.getCity());
+        assertEquals(addressCreateDTO.getNumber(), addressCreated.getNumber());
+        assertEquals(addressCreateDTO.getZipCode(), addressCreated.getZipCode());
+        assertEquals(addressCreateDTO.getState(), addressCreated.getState());
+        assertEquals(user.getId(), addressCreated.getUserId());
+    }
+
 
 }
