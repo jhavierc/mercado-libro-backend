4076ce7e7cccd55b3717d74a598c5d7a75db6797
LihuelMujica <lihuelmujica@gmail.com>
2023-11-30 16:31:35 -0300

    feat: add address entity

---
+++ src/main/java/com/mercadolibro/entity/Address.java
@@ -0,0 +1,33 @@
+package com.mercadolibro.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    private String street;
+    @NotNull
+    private Integer number;
+    @NotNull
+    private String city;
+    @NotNull
+    private String zipCode;
+    private String district;
+    private String department;
+    @Column(name = "user_id")
+    @NotNull
+    private Integer userId;
+}
