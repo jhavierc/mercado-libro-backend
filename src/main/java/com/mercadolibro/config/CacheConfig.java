22c54097a835a43c495f7b790fffe1c3625ce1be
Carlos Cepeda <kharlosj@gmail.com>
2023-12-06 19:57:34 -0500

    inactive cache

--- src/main/java/com/mercadolibro/config/CacheConfig.java
+++ src/main/java/com/mercadolibro/config/CacheConfig.java
@@ -10,8 +10,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CacheConfig {
 
-    @Bean
+    /*@Bean
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("books");
-    }
+    }*/
 }
